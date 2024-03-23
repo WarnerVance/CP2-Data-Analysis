482a2fc3af929f9ac548a954d3d54c55f44ec623
Warner Vance <warnervance@icloud.com>
2024-03-22 22:55:03 -0500

    Removed time package

--- test.py
+++ test.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import plotly.express as px
-import time
-start = time.process_time()
+
 df = pd.read_csv('Crimes_-_Map.csv')
 
 df_arrests = df[df["ARREST"] == "Y"]
@@ -21,6 +20,5 @@ fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
 fig.show()
 
 print(f"Size: {df.shape[0]}")
-print(f"Time: {time.process_time() - start}")
 print("test.py executed successfully!")
 
